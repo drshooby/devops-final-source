f69d2001295d6264e16bf47aa64afc92e3e84823
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-04-30 11:53:41 -0700

    feat: use txt

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@ module.exports = {
         '@semantic-release/github',
         {
           assets: [
-            { path: 'version.json', label: 'Version JSON' },
+            { path: 'VERSION.txt', label: 'Version TXT' },
           ],
           failComment: false,  // Disable creation of failure comments
           failTitle: false,    // Disable creation of failure PRs
@@ -18,14 +18,13 @@ module.exports = {
       [
         '@semantic-release/exec',
         {
-          // Create a version.json file containing version info and environment
-          prepareCmd: 'node -e "const fs=require(\'fs\'); fs.writeFileSync(\'version.json\', JSON.stringify({version: \'${nextRelease.version}-${process.env.ENVIRONMENT}\', environment: \'${process.env.ENVIRONMENT}\', releaseDate: new Date().toISOString()}, null, 2))"',
+          prepareCmd: "echo '${nextRelease.version}-${process.env.ENVIRONMENT}' > VERSION.txt",
         },
       ],
       [
         '@semantic-release/git',
         {
-          assets: ['version.json', 'package.json'],
+          assets: ['VERSION.txt', 'package.json'],
           message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
         },
       ],
